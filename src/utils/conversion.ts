473ce09ed33018d44a34d7bdeb775a70bb93f120
Lucas Bertola <lucasbertola@gmail.com>
2025-05-03 16:48:51 +0200

    build: mise à jour du site avec la nouvelle valeur minimale pour Chess.com

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -5,30 +5,42 @@
  * 
  
 
-
+une personne qui aurait 1500elo sur lichess, il serait quel elo sur chess? en se basant sur la precision pour faire la correspondance.
+fait un programme pour le savoir
+------
 Fait moi le grapique ou j'ai en abicesse le elo chesscom, et en ordonné le elo lichess
 Fait commencer chesscom a 150elo et fait le finir a 2500
 ----
-
-Fait moi l'approximation d'un polynome au 18ieme degres de cette fonction
+Fait moi l'approximation d'un polynome au 20ieme degres de cette fonction
 Et donne moi sa fonction.
-Donne moi tous les numeros apres la virgule des coefficiants. Je veux au moins une precision a 25 chiffres. affiche sous forme de array de nombre 
-
-(hesite pas a utiliser le type decimal.Decimal  pour avoir assez de precision)
-
+Donne moi tous les numeros apres la virgule des coefficiants. Je veux au moins une precision a 25 chiffres. affiche sous forme de array de nombre Decimal
+Modifie bien la fonction str, pour dire d'ecrire la precision de 25
  */
 
 export const convertLichessToChesscom = (lichessRating: number): number => {
   const x = lichessRating;
   return (
-    -1.564344693910447635e-19 * Math.pow(x, 7) +
-    1.918172896212396419e-15 * Math.pow(x, 6) +
-    -9.604852714700698433e-12 * Math.pow(x, 5) +
-    2.531070597589738935e-8 * Math.pow(x, 4) +
-    -3.776776683098590279e-5 * Math.pow(x, 3) +
-    3.203797711117362806e-2 * Math.pow(x, 2) +
-    -1.337007834172812082e1 * x +
-    2.250012050866800109e3
+    -2.353783698019859e-57 * Math.pow(x, 20) +
+    4.543994430347881e-53 * Math.pow(x, 19) +
+    -3.6749705003365024e-49 * Math.pow(x, 18) +
+    1.525640443640019e-45 * Math.pow(x, 17) +
+    -2.7831036709048628e-42 * Math.pow(x, 16) +
+    -2.263041711376188e-39 * Math.pow(x, 15) +
+    1.967452930593192e-35 * Math.pow(x, 14) +
+    -1.6299316927477007e-32 * Math.pow(x, 13) +
+    -9.304397186722809e-29 * Math.pow(x, 12) +
+    2.4960268912735315e-25 * Math.pow(x, 11) +
+    8.997960261194114e-23 * Math.pow(x, 10) +
+    -1.750488332570652e-18 * Math.pow(x, 9) +
+    4.709470715840608e-15 * Math.pow(x, 8) +
+    -7.368644819116138e-12 * Math.pow(x, 7) +
+    7.816473365764003e-9 * Math.pow(x, 6) +
+    -5.869047133300792e-6 * Math.pow(x, 5) +
+    0.0031343669851908607 * Math.pow(x, 4) +
+    -1.1661696185208208 * Math.pow(x, 3) +
+    287.264176976182 * Math.pow(x, 2) +
+    -42060.07938610531 * x +
+    2766724.686389653
   );
 };
 
